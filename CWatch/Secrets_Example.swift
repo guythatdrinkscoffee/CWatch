444603f884355c45ae4f9888bc91dce33ced95c0
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-12-09 17:54:23 -0800

    Added secrets example.

---
+++ CWatch/Secrets_Example.swift
@@ -0,0 +1,30 @@
+//
+//  Secrets_Example.swift
+//  CWatch
+//
+//  Created by J Manuel Zaragoza on 12/9/22.
+//
+
+import Foundation
+
+
+/**
+    Create a file called Secrets.swift with the following
+    struct as an example.
+    
+    struct Secrets {
+        static let apiKey = ...
+        static let apiHost = ...
+    }
+ 
+    You can either use RapidApi or get an api key from
+    Coinranking directly.
+ */
+
+struct Secrets_Example {
+    static let apiKey = "Your rapid api key."
+    static let apiHost = "Your rapid api host for the Coinranking api."
+}
+
+
+
